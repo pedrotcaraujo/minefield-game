597cb6d48809b05646305ac222bde39e0e85c837
Pedro Araujo <pedrotcaraujo@gmail.com>
2013-04-25 13:33:57 -0300

    Code refactor

--- code/minefield.js
+++ code/minefield.js
@@ -2,7 +2,6 @@
 function Minefield () {
 
 //	Declaring constant
-const FIELD = $('#field');
 var mines;
 var row;
 var col;
@@ -13,51 +12,53 @@ this.init = function(typeGame) {
 
 	cleanField();
 
-//	Compare type selected by user, if is 8x8, 16x16 or custom game
-if (typeGame=="8") {
+	//	Compare type selected by user, if is 8x8, 16x16 or custom game
+	if (typeGame==="8") {
 
-	row = 8;
-	col = 8;
-	mines = 10;
+		row = 8;
+		col = 8;
+		mines = 10;
 
-	fieldsArray = buildArray(row,col);
+		fieldsArray = buildArray(row,col);
 
-} else if (typeGame="16") {
+	} else if (typeGame==="16") {
 
-	row = 16;
-	col = 16;
-	mines = 40;
+		row = 16;
+		col = 16;
+		mines = 40;
 
-	fieldsArray = buildArray(row,col);
+		fieldsArray = buildArray(row,col);
 
-} else if (typeGame=="custom") {
+	} 
+// Create a custom game
+//	else if (typeGame==="custom") {
 
-	row = parseInt(document.getElementById('field-row'));
-	col = parseInt(document.getElementById('field-col'));
-	mines = parseInt(document.getElementById('field-mines'));
+//		row = parseInt(document.getElementById('field-row'));
+//		col = parseInt(document.getElementById('field-col'));
+//		mines = parseInt(document.getElementById('field-mines'));
 
-	fieldsArray = buildArray(row,col);			
-}
+//		fieldsArray = buildArray(row,col);			
+//	}
 
 
-addBomb();
-calcSideFields();
-mountField();
+	addBomb();
+	calcSideFields();
+	mountField();
 
-}
+};
 
 //	Function that will mount the minefield and insert 0 into each field.
 function mountField () {
 
 	for (var i = 0; i < row; i++) {
 		if (i>0) {
-			FIELD.append('<br>');
+			$('#field').append('<br>');
 		}
 		for (var j = 0; j < col; j++) {
-			if (fieldsArray[i][j] == "*") {
-				FIELD.append("<span class='mine-field' style='color:red'>"+fieldsArray[i][j]+"</span>");
+			if (fieldsArray[i][j] === "*") {
+				$('#field').append("<span class='mine-field' style='color:red'>"+fieldsArray[i][j]+"</span>");
 			} else{
-				FIELD.append("<span class='mine-field'>"+fieldsArray[i][j]+"</span>");				
+				$('#field').append("<span class='mine-field'>"+fieldsArray[i][j]+"</span>");				
 			}		
 		}
 
@@ -76,7 +77,7 @@ function addBomb () {
 		var randomnumber = Math.ceil(Math.random()*amountFields);
 		var found = false;
 		for (var i=0; i < bombs.length; i++) {
-			if (bombs[i] == randomnumber) {
+			if (bombs[i] === randomnumber) {
 				found = true;
 				break;
 			}
@@ -90,18 +91,18 @@ function addBomb () {
 //		}
 	}
 
-		for (var i = 0; i < row; i++) {
+		for (var index = 0; index < row; index++) {
 			for (var j = 0; j < col; j++) {
 				
 				for (var b = 0; b < bombs.length; b++) {
-					if (bombs[b] == indexBomb) {
-						fieldsArray[i][j] = "*";
-					} else if (fieldsArray[i][j] != "*") {
-						fieldsArray[i][j] = 0;
+					if (bombs[b] === indexBomb) {
+						fieldsArray[index][j] = "*";
+					} else if (fieldsArray[index][j] !== "*") {
+						fieldsArray[index][j] = 0;
 					}
 				}
 
-				indexBomb++
+				indexBomb++;
 			}
 		}
 
@@ -111,47 +112,47 @@ function addBomb () {
 function calcSideFields () {
 	for (var i = 0; i < row; i++) {
 		for (var j = 0; j < col; j++) {
-			if (fieldsArray[i][j] == "*") {
+			if (fieldsArray[i][j] === "*") {
 //	Check adjacent fields on down row
 				if (i<row-1) {
-					if (fieldsArray[i+1][j] != "*") {
-					fieldsArray[i+1][j] = fieldsArray[i+1][j]+1
+					if (fieldsArray[i+1][j] !== "*") {
+					fieldsArray[i+1][j] = fieldsArray[i+1][j]+1;
 					}
 					if (j<col-1) {
-						if (fieldsArray[i+1][j+1] != "*") {
-						fieldsArray[i+1][j+1] = fieldsArray[i+1][j+1]+1
+						if (fieldsArray[i+1][j+1] !== "*") {
+						fieldsArray[i+1][j+1] = fieldsArray[i+1][j+1]+1;
 						}
 					}
 					if (j>0) {
-						if (fieldsArray[i+1][j-1] != "*") {
-						fieldsArray[i+1][j-1] = fieldsArray[i+1][j-1]+1
+						if (fieldsArray[i+1][j-1] !== "*") {
+						fieldsArray[i+1][j-1] = fieldsArray[i+1][j-1]+1;
 						}
 					}
 				}
 //	Check adjacent fields on up row
 				if (i>0) {
-					if (fieldsArray[i-1][j] != "*") {
-					fieldsArray[i-1][j] = fieldsArray[i-1][j]+1
+					if (fieldsArray[i-1][j] !== "*") {
+					fieldsArray[i-1][j] = fieldsArray[i-1][j]+1;
 					}
 					if (j<col-1) {
-						if (fieldsArray[i-1][j+1] != "*") {
-						fieldsArray[i-1][j+1] = fieldsArray[i-1][j+1]+1
+						if (fieldsArray[i-1][j+1] !== "*") {
+						fieldsArray[i-1][j+1] = fieldsArray[i-1][j+1]+1;
 						}
 					}
 					if (j>0) {
-						if (fieldsArray[i-1][j-1] != "*") {
-						fieldsArray[i-1][j-1] = fieldsArray[i-1][j-1]+1
+						if (fieldsArray[i-1][j-1] !== "*") {
+						fieldsArray[i-1][j-1] = fieldsArray[i-1][j-1]+1;
 						}
 					}
 				}
 //	Check adjacent fields same row
 				if (j<col-1) {
-					if (fieldsArray[i][j+1] != "*") {
+					if (fieldsArray[i][j+1] !== "*") {
 					fieldsArray[i][j+1] = fieldsArray[i][j+1]+1;
 					}
 				}
 				if (j>0) {
-					if (fieldsArray[i][j-1] != "*") {
+					if (fieldsArray[i][j-1] !== "*") {
 					fieldsArray[i][j-1] = fieldsArray[i][j-1]+1;
 					}
 				}
@@ -173,7 +174,7 @@ function buildArray (row, col) {
 }
 
 function cleanField () {
-	FIELD.html('');
+	$('#field').html('');
 }
 
 
